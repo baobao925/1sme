3d837f0a9351be5f8a0da134cb7bcd91739ed983
Khoatv11 <vtitba@gmail.com>
2026-05-08 15:05:08 +0700

    q

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // Cấu hình đường dẫn tương đối cho trang tĩnh (GitHub Pages)
   server: {
     host: "::",
     port: 8080,
